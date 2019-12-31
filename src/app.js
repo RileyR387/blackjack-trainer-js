2a73ff9457f97208015446d6ba55943432bd9713
Riley Raschke <riley@rrappsdev.com>
2019-12-30 20:06:27 -0600

    Initial project

---
+++ src/app.js
@@ -0,0 +1,14 @@
+
+var BlackjackAI = angular.module('BlackjackAI', []);
+
+BlackjackAI.controller( 'BlackjackGameController', ['$scope', 'BlackjackGameService', function( $scope, BlackjackGameService ) {
+  var self = this;
+}]);
+
+BlackjackAI.factory('BlackjackGameService', [ '$q', function( $q ){
+  var factory = {
+
+  };
+  return factory;
+}]);
+
