a67a52c9a1c0178bedcaa72aed3da31010423211
Riley Raschke <riley@rrappsdev.com>
2019-12-31 15:09:38 -0600

    Initial mockup stacking cards and handling doubles

--- src/app.js
+++ src/app.js
@@ -3,6 +3,14 @@ var BlackjackAI = angular.module('BlackjackAI', []);
 
 BlackjackAI.controller( 'BlackjackGameController', ['$scope', 'BlackjackGameService', function( $scope, BlackjackGameService ) {
   var self = this;
+
+  self.game = {
+    opts: null,
+    players: null,
+    gameState = null,
+    shoe = null
+  };
+
 }]);
 
 BlackjackAI.factory('BlackjackGameService', [ '$q', function( $q ){
