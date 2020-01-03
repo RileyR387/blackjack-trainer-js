8079c71f1d2060afff0ee5ce262d49d797398914
Riley Raschke <riley@rrappsdev.com>
2020-01-02 23:27:26 -0600

    binding's to modals! But only 1...

--- src/app.js
+++ src/app.js
@@ -4,26 +4,101 @@
  * OSL-3.0
  */
 
-$('#gSettingsModal').modal({show: true});
+//$('#gSettingsModal').modal({show: true});
+var app = angular.module('BlackjackAI', []);
 
-var BlackjackAI = angular.module('BlackjackAI', []);
+var GameSettingsDialogModel = function() {
+  this.visible = false;
+};
+GameSettingsDialogModel.prototype.open = function(opts) {
+  this.opts = opts;
+  this.visible = true;
+};
+GameSettingsDialogModel.prototype.close = function() {
+  this.visible = false;
+};
 
-BlackjackAI.controller( 'BlackjackGameController', ['$scope', 'BlackjackGameService', function( $scope, BlackjackGameService ) {
-  var self = this;
+var PlayerSettingsDialogModel = function() {
+  this.visible = false;
+};
+PlayerSettingsDialogModel.prototype.open = function(opts) {
+  this.opts = opts;
+  this.visible = true;
+};
+PlayerSettingsDialogModel.prototype.close = function() {
+  this.visible = false;
+};
 
-  self.game = {
-    opts: null,
+app.controller( 'BlackjackGameController', ['$scope', 'BlackjackGameService', function( $scope, BlackjackGameService ) {
+  $scope.gameSettingsDialog   = new GameSettingsDialogModel();
+  $scope.playerSettingsDialog = new PlayerSettingsDialogModel();
+  this.game = {
+    opts: {
+      deckCount: 6,
+      dealRate: 0.8,
+      showDeckStats: true,
+      payout: 1.5
+    },
     players: null,
     gameState: null,
     shoe: null
   };
-
 }]);
 
-BlackjackAI.factory('BlackjackGameService', [ '$q', function( $q ){
-  var factory = {
-
+app.directive('gameSettingsDialog', [function() {
+  return {
+    restrict: 'E',
+    scope: {
+      model: '=',
+    },
+    link: function(scope, element, attributes) {
+      scope.$watch('model.visible', function(newValue) {
+        var modalElement = element.find('#gSettingsModal');
+        $(modalElement).modal(newValue ? 'show' : 'hide');
+      });
+      element.on('shown.bs.modal', function() {
+        scope.$apply(function() {
+          scope.model.visible = true;
+        });
+      });
+      element.on('hidden.bs.modal', function() {
+        scope.$apply(function() {
+          scope.model.visible = false;
+        });
+      });
+    },
+    templateUrl: 'game-settings-dialog.html',
   };
+}]);
+
+app.directive('playerSettingsDialog', [function() {
+  return {
+    restrict: 'E',
+    scope: {
+      model: '=',
+    },
+    link: function(scope, element, attributes) {
+      scope.$watch('model.visible', function(newValue) {
+        var modalElement = element.find('#pSettingsModal');
+        $(modalElement).modal(newValue ? 'show' : 'hide');
+      });
+      element.on('shown.bs.modal', function() {
+        scope.$apply(function() {
+          scope.model.visible = true;
+        });
+      });
+      element.on('hidden.bs.modal', function() {
+        scope.$apply(function() {
+          scope.model.visible = false;
+        });
+      });
+    },
+    templateUrl: 'player-settings-dialog.html',
+  };
+}]);
+
+app.factory('BlackjackGameService', [ '$q', function( $q ){
+  var factory = { };
   return factory;
 }]);
 
